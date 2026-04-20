9edf919fec93e3486b0b60c0cc38ab92c9a0fd61
Indulekha <indulekha0606@gmail.com>
2026-04-20 19:45:47 +0530

    Initial portfolio commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Simple fade-in effect on load
+window.addEventListener('load', () => {
+    document.body.style.opacity = '0';
+    setTimeout(() => {
+        document.body.style.transition = 'opacity 1s ease-in-out';
+        document.body.style.opacity = '1';
+    }, 100);
+});
